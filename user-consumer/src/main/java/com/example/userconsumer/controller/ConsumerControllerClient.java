1f46013c6df86c9779986b540da1fa30f51c1cea
Yechan <packerstorm@gmail.com>
2020-12-18 11:09:05 +0900

    Ribbon 적용을 위한 dependency 및 로드 밸런서 사용 메소드 추가

--- user-consumer/src/main/java/com/example/userconsumer/controller/ConsumerControllerClient.java
+++ user-consumer/src/main/java/com/example/userconsumer/controller/ConsumerControllerClient.java
@@ -3,27 +3,33 @@ package com.example.userconsumer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.List;
 
-@Controller
+//@Controller
+@RestController
 public class ConsumerControllerClient {
 
     @Autowired
-    private DiscoveryClient discoveryClient;
+//    private DiscoveryClient discoveryClient;
+    private LoadBalancerClient loadBalancerClient; // for load balancer
 
+    @GetMapping("/eureka/client")
     public void getUser() throws RestClientException, IOException {
 
 //        String baseUrl = "http://localhost:8080/user"; // 기존 서비스 호출 url
 
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-producer");
-        ServiceInstance serviceInstance = instances.get(0);
-
+//        List<ServiceInstance> instances = discoveryClient.getInstances("user-producer");
+//        ServiceInstance serviceInstance = instances.get(0);
+        ServiceInstance serviceInstance = loadBalancerClient.choose("user-producer"); // for load balancer
         String baseUrl = serviceInstance.getUri().toString() + "/user";
         System.out.println("baseUrl: " + baseUrl);
 
